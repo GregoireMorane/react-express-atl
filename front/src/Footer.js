e5cfed15cd950bbdae6e42e20f6b5a91849e18b9
GrégoireMorane <gh.morane@gmail.com>
2019-02-25 14:56:50 +0100

    ++

---
+++ front/src/Footer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Footer extends Component {
+
+	render() {
+	  return (
+		<div>
+			<p style={{fontSize:10}}>Copyright oklmLaWild</p>
+		</div>
+	  );
+	}
+  }
+  
+  export default Footer;
